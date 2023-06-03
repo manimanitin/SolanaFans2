14c607e71524953d2a3980c8331086ff9d836fee
manimanitin <andreman1781@gmail.com>
2023-06-03 10:28:43 -0600

    c conexiona shyft sdl

--- solanafans/pages/api/shyft.js
+++ solanafans/pages/api/shyft.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 export default async function handler(req, res) {
     try {
         // Realizar la llamada a la API de Shyft
-        const response = await axios.get('https://api.shyft.com/endpoint', { "x-api-key": process.env.shyftkey });
-
+        console.log(req.publicKey?req.publicKey:'valio');
+        const response = await axios.get(`https://api.shyft.to/sol/v1/wallet/balance?network=testnet&wallet=${req.publicKey}`, { "x-api-key": process.env.shyftkey });
         // Procesar la respuesta de la API de Shyft
         const data = response.data;
 
