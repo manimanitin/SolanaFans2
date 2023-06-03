fb55cc407515d328faefac29ecf28d1133e96fde
manimanitin <andreman1781@gmail.com>
2023-06-02 20:02:05 -0600

    añadir dotenv y shyftkey

--- solanafans/pages/api/shyft.js
+++ solanafans/pages/api/shyft.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  try {
-    // Realizar la llamada a la API de Shyft
-    const response = await axios.get('https://api.shyft.com/endpoint');
+    try {
+        // Realizar la llamada a la API de Shyft
+        const response = await axios.get('https://api.shyft.com/endpoint', { "x-api-key": process.env.shyftkey });
 
-    // Procesar la respuesta de la API de Shyft
-    const data = response.data;
+        // Procesar la respuesta de la API de Shyft
+        const data = response.data;
 
-    // Devolver los datos obtenidos como respuesta de la API de Next.js
-    res.status(200).json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Ha ocurrido un error al llamar a la API de Shyft' });
-  }
+        // Devolver los datos obtenidos como respuesta de la API de Next.js
+        res.status(200).json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Ha ocurrido un error al llamar a la API de Shyft' });
+    }
 }
